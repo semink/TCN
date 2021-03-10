0b67922b1b3755c69b36e7baf41e2dbbe1203818
Semin <semin.kwak@epfl.ch>
2021-03-10 22:12:07 +0100

    [UPDATE] logging format

--- TCN/low_resolution/test.py
+++ TCN/low_resolution/test.py
@@ -75,7 +75,7 @@ model = LowResolutionTCN(input_dim, compress_dim, seq_length, num_channels,
                          kernel_size=kernel_size, dropout=dropout)
 
 writer = SummaryWriter()
-writer.add_graph(model)
+writer.add_graph(model, x_valid[:1, :, :])
 
 if torch.cuda.is_available():
     model.cuda()
